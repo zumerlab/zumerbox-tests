3a32929ccc3e0f35c42d9584cc50b6ee296a127a
Juan Martin <martinmuda@gmail.com>
2024-03-24 22:58:45 +0300

    Fix bin execution

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,13 @@ const { execSync } = require('child_process')
 const path = require('path')
 try {
   const args = process.argv.slice(2).join(' ')
+  console.log('hola')
+  const jestCmd = path.resolve(__dirname, '../node_modules/.bin/jest');
   const configPath = path.resolve(__dirname, '../jest.config.json')
   const result = execSync(`jest -c ${configPath} ${args}`, {
     stdio: 'inherit'
   })
 
-  // if (result === null) console.log('Lint SCSS sin errores!!')
 } catch (error) {
   console.log('Jest ended with errors. Fix and start again')
 }
